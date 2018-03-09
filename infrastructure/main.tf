a19dd824923ef0a9b7c5dbcf9bab2367a3244575
Mike <mike.suggitt>
2018-03-09 12:25:49 +0000

    - Initial commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,41 @@
+locals {
+  app_full_name = "${var.product}-${var.app_name}-${var.app_type}"
+  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+}
+# "${local.ase_name}"
+# "${local.app_full_name}"
+
+module "app" {
+  source = "git@github.com:contino/moj-module-webapp?ref=master"
+  product = "${local.app_full_name}"
+  location = "${var.location}"
+  env = "${var.env}"
+  ilbIp = "${var.ilbIp}"
+  subscription = "${var.subscription}"
+  is_frontend = true
+
+  app_settings = {
+    # REDIS_HOST = "${module.redis-cache.host_name}"
+    # REDIS_PORT = "${module.redis-cache.redis_port}"
+    # REDIS_PASSWORD = "${module.redis-cache.access_key}"
+    # RECIPE_BACKEND_URL = "http://rhubarb-recipe-backend-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+    WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
+
+    # NODE_ENV = "${var.env}"
+    # PORT = "8080"
+
+    # logging vars & healthcheck
+    REFORM_SERVICE_NAME = "${local.app_full_name}"
+    REFORM_TEAM = "${var.team_name}"
+    REFORM_SERVICE_TYPE = "${var.app_language}"
+    REFORM_ENVIRONMENT = "${var.env}"
+
+    PACKAGES_NAME = "${local.app_full_name}"
+    PACKAGES_PROJECT = "${var.team_name}"
+    PACKAGES_ENVIRONMENT = "${var.env}"
+
+    ROOT_APPENDER = "${var.root_appender}"
+    JSON_CONSOLE_PRETTY_PRINT = "${var.json_console_pretty_print}"
+    LOG_OUTPUT = "${var.log_output}"
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,66 @@
+variable "product" {
+  type = "string"
+  default = "jui"
+  description = "The name of your application"
+}
+
+variable "app_name" {
+  default = "webapp"
+}
+
+variable "app_type" {
+  default = "web"
+}
+
+variable "team_name" {
+  default = "judicial_ui"
+}
+
+variable "app_language" {
+  default = "node"
+}
+
+variable "location" {
+  type = "string"
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+  description = "(Required) The environment in which to deploy the application infrastructure."
+}
+
+variable "subscription" {
+  type = "string"
+}
+
+variable "ilbIp"{
+
+}
+
+variable "tenant_id" {
+
+}
+
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+
+
+
+////////////////////////////////////////////////
+// Logging
+////////////////////////////////////////////////
+variable "root_appender" {
+  default = "JSON_CONSOLE"
+}
+
+variable "json_console_pretty_print" {
+  default = "false"
+}
+
+variable "log_output" {
+  default = "single"
+}

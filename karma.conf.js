b194a0bff0af95e98cb23480f475e35404f33b6e
mike-suggitt <mike.suggitt>
2018-03-15 17:23:44 +0000

    - Update sonar stuff

--- karma.conf.js
+++ karma.conf.js
@@ -5,10 +5,13 @@ module.exports = function (config) {
     config.set({
         basePath: '',
         frameworks: ['jasmine', '@angular/cli'],
-        files: ['src/sonar/*.ts'],
+        // files: [
+        //     'src/sonar/*.ts'
+        // ],
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
+            require("karma-phantomjs-launcher"),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
             require('@angular/cli/plugins/karma')
@@ -28,7 +31,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         singleRun: false
     });
 };

9d9bc7f062191f2e22a379000760d3d42c8e56f3
mike-suggitt <mikessuggitt@gmail.com>
2018-04-24 08:50:04 +0100

    - Fixed karma dep issue  - Updated test script

--- karma.conf.js
+++ karma.conf.js
@@ -23,9 +23,9 @@ module.exports = function (config) {
             dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
         },
-        angularCli: {
-            environment: 'dev'
-        },
+        // angularCli: {
+        //     environment: 'dev'
+        // },
         reporters: ['progress', 'kjhtml'],
         port: 9876,
         colors: true,

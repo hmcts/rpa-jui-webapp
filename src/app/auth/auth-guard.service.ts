9387d34d2a55c079ceaf74322cd253e6fe1e0479
mike-suggitt <mikessuggitt@gmail.com>
2018-06-08 16:59:48 +0100

    - Updated config for callback

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -3,7 +3,6 @@ import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 import {ConfigService} from "../config.service";
 import {DOCUMENT} from '@angular/common';
-import * as config from '../../../config';
 
 @Injectable({
     providedIn: 'root'
@@ -20,7 +19,7 @@ export class AuthGuardService {
     generateLoginUrl() {
         const base = this.configService.config.services.idam_web;
         const clientId = 'jui_webapp';
-        const callback = config.oauth_callback_url;
+        const callback = this.configService.config.oauth_callback_url;
         return `${base}?response_type=code&client_id=${clientId}&redirect_uri=${callback}`;
     }
 

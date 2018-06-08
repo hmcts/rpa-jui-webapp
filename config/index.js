ff493e6f6d0e78678ad3eb02a51b6333442e284e
mike-suggitt <mikessuggitt@gmail.com>
2018-06-08 14:47:44 +0100

    - Added saat config to index

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 const config = {
     docker: require('./docker.config.js'),
     local: require('./local.config.js'),
-    aat: require('./aat.config.js')
+    aat: require('./aat.config.js'),
+    saat: require('./saat.config.js')
 };
 const env = typeof(process) !== 'undefined' ? (process.env.JUI_ENV || 'local') : 'local';
 console.log('Using ' + env + ' Config');

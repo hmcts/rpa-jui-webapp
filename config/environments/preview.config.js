aed33cd61c179893b95c901e9a82013fded65418
Alec Doran-Twyford <alec@alectronic.co>
2018-06-27 16:07:47 +0100

    preview configs added

---
+++ config/environments/preview.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    services: {
+        ccd_data_api: "https://ccd-data-store-api-aat.service.core-compute-aat.internal",
+        idam_web: "http://idam-web-public-idam-aat.service.core-compute-aat.internal",
+        idam_api: "http://idam-api-idam-aat.service.core-compute-aat.internal",
+        s2s: "http://rpe-service-auth-provider-aat.service.core-compute-aat.internal",
+        dm_store_api: "http://dm-store-aat.service.core-compute-aat.internal",
+        em_anno_api: "http://em-anno-aat.service.core-compute-aat.internal",
+        em_redact_api: "http://em-redact-aat.service.core-compute-aat.internal",
+        coh_cor_api: "http://coh-cor-aat.service.core-compute-aat.internal"
+    },
+    api_base_url: "https://jui-webapp-aat.service.core-compute-aat.internal",
+    oauth_callback_url: "https://jui-webapp-aat.service.core-compute-aat.internal/oauth2/callback",
+    useProxy: false
+};
---
+++ config/environments/spreview.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    services: {
+        ccd_data_api: "https://ccd-data-store-api-saat.service.core-compute-saat.internal",
+        idam_web: "https://idam-web-public-idam-saat.service.core-compute-saat.internal",
+        idam_api: "https://idam-api-idam-saat.service.core-compute-saat.internal",
+        s2s: "http://rpe-service-auth-provider-saat.service.core-compute-saat.internal",
+        dm_store_api: "http://dm-store-saat.service.core-compute-saat.internal",
+        em_anno_api: "http://em-anno-saat.service.core-compute-saat.internal",
+        em_redact_api: "http://em-redact-saat.service.core-compute-saat.internal",
+        coh_cor_api: "http://coh-cor-saat.service.core-compute-saat.internal"
+    },
+    api_base_url: "https://jui-webapp-saat.service.core-compute-saat.internal",
+    oauth_callback_url: "https://jui-webapp-saat.service.core-compute-saat.internal/oauth2/callback",
+    useProxy: false
+};

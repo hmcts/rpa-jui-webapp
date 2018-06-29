a950fa6bac2fc307f4cf769f61aa80dadd75d50e
mike-suggitt <mikessuggitt@gmail.com>
2018-06-29 16:28:27 +0100

    - Adding step defs

--- test/e2e/features/step_definitions/navigation.steps.js
+++ test/e2e/features/step_definitions/navigation.steps.js
@@ -1,5 +1,11 @@
 const {Given, When, Then} = require('cucumber');
 
+Given(/^I navigate to "(.*)"/, async function (url) {
+    await browser.get(url);
+    await browser.driver.manage().deleteAllCookies();
+    await browser.refresh();
+});
+
 Given(/^I navigate to JUI$/, async function () {
     await browser.get(this.config.serverUrls[this.config.targetEnv]);
     await browser.driver.manage().deleteAllCookies();

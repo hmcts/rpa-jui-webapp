91488377395190ae3982e95a6c875f54612be89a
Alec Doran-Twyford <alec@alectronic.co>
2018-07-24 12:26:27 +0100

    stage one of removing old vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,47 +1,51 @@
 locals {
-  app_full_name = "${var.product}-${var.component}"
-  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  vault_name = "${var.shared_product_name}-${local.local_env}"
+    app_full_name = "${var.product}-${var.component}"
+    ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+    local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+    vault_name = "${var.shared_product_name}-${local.local_env}"
+}
+
+provider "vault" {
+    address = "https://vault.reform.hmcts.net:6200"
 }
 
 module "app" {
-  source = "git@github.com:hmcts/moj-module-webapp?ref=master"
-  product = "${local.app_full_name}"
-  location = "${var.location}"
-  env = "${var.env}"
-  ilbIp = "${var.ilbIp}"
-  subscription = "${var.subscription}"
-  capacity     = "${var.capacity}"
-  is_frontend = true
-  additional_host_name = "${local.app_full_name}-${var.env}.service.${var.env}.platform.hmcts.net"
-  https_only="false"
-  common_tags  = "${var.common_tags}"
+    source = "git@github.com:hmcts/moj-module-webapp?ref=master"
+    product = "${local.app_full_name}"
+    location = "${var.location}"
+    env = "${var.env}"
+    ilbIp = "${var.ilbIp}"
+    subscription = "${var.subscription}"
+    capacity     = "${var.capacity}"
+    is_frontend = true
+    additional_host_name = "${local.app_full_name}-${var.env}.service.${var.env}.platform.hmcts.net"
+    https_only="false"
+    common_tags  = "${var.common_tags}"
 
-  app_settings = {
-    # REDIS_HOST = "${module.redis-cache.host_name}"
-    # REDIS_PORT = "${module.redis-cache.redis_port}"
-    # REDIS_PASSWORD = "${module.redis-cache.access_key}"
-    # RECIPE_BACKEND_URL = "http://rhubarb-recipe-backend-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
-    WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
+    app_settings = {
+        # REDIS_HOST = "${module.redis-cache.host_name}"
+        # REDIS_PORT = "${module.redis-cache.redis_port}"
+        # REDIS_PASSWORD = "${module.redis-cache.access_key}"
+        # RECIPE_BACKEND_URL = "http://rhubarb-recipe-backend-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+        WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
 
-    # NODE_ENV = "${var.env}"
-    # PORT = "8080"
-    JUI_ENV = "${var.env}"
+        # NODE_ENV = "${var.env}"
+        # PORT = "8080"
+        JUI_ENV = "${var.env}"
 
-    # logging vars & healthcheck
-    REFORM_SERVICE_NAME = "${local.app_full_name}"
-    REFORM_TEAM = "${var.team_name}"
-    REFORM_SERVICE_TYPE = "${var.app_language}"
-    REFORM_ENVIRONMENT = "${var.env}"
+        # logging vars & healthcheck
+        REFORM_SERVICE_NAME = "${local.app_full_name}"
+        REFORM_TEAM = "${var.team_name}"
+        REFORM_SERVICE_TYPE = "${var.app_language}"
+        REFORM_ENVIRONMENT = "${var.env}"
 
-    PACKAGES_NAME = "${local.app_full_name}"
-    PACKAGES_PROJECT = "${var.team_name}"
-    PACKAGES_ENVIRONMENT = "${var.env}"
+        PACKAGES_NAME = "${local.app_full_name}"
+        PACKAGES_PROJECT = "${var.team_name}"
+        PACKAGES_ENVIRONMENT = "${var.env}"
 
-    JUI_S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-    IDAM_SECRET = "${data.azurerm_key_vault_secret.oauth2_secret.value}"
-  }
+        JUI_S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+        IDAM_SECRET = "${data.azurerm_key_vault_secret.oauth2_secret.value}"
+    }
 }
 
 data "azurerm_key_vault" "key_vault" {

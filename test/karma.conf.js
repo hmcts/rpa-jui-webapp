8d4866eb34391a0cdd51e332e6c4a8a7c0fb7553
Alec Doran-Twyford <alec@alectronic.co>
2018-07-26 13:56:47 +0100

    re-enabling codecov (#77)

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,30 +1,33 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+const karmaJasmine = require('karma-jasmine');
+const karmaChromeLauncher = require('karma-chrome-launcher');
+const karmaPhantomjsLauncher = require('karma-phantomjs-launcher');
+const karmaJasmineHtmlReporter = require('karma-jasmine-html-reporter');
+const karmaCoverageIstanbulReporter = require('karma-coverage-istanbul-reporter');
+const generated = require('@angular-devkit/build-angular/plugins/karma');
+const { join } = require('path');
+
+module.exports = config => {
     config.set({
         basePath: '',
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
-        // files: [
-        //     'src/sonar/*.ts'
-        // ],
         plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-phantomjs-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma')
+            karmaJasmine,
+            karmaChromeLauncher,
+            karmaPhantomjsLauncher,
+            karmaJasmineHtmlReporter,
+            karmaCoverageIstanbulReporter,
+            generated
         ],
-        client: { clearContext: false // leave Jasmine Spec Runner output visible in browser
+        client: {
+            clearContext: false
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
+            dir: join('.', 'reports', 'tests', 'coverage'), reports: ['html', 'lcovonly'],
             fixWebpackSourcePaths: true
         },
-        // angularCli: {
-        //     environment: 'dev'
-        // },
         reporters: ['progress', 'kjhtml'],
         port: 9876,
         colors: true,

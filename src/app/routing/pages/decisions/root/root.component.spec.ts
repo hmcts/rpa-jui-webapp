3756bf050b51f0c1950449cf07bb2d13f7ccf45e
mike-suggitt <mikessuggitt@gmail.com>
2018-07-30 14:58:53 +0100

    - Added unit tests for root components

---
+++ src/app/routing/pages/decisions/root/root.component.spec.ts
@@ -0,0 +1,53 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {DecisionRootComponent} from './root.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {DomainModule} from '../../../../domain/domain.module';
+import {SharedModule} from '../../../../shared/shared.module';
+import {Observable} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+
+describe('DecisionRootComponent', () => {
+    let component: DecisionRootComponent;
+    let fixture: ComponentFixture<DecisionRootComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                DecisionRootComponent
+            ],
+            imports: [DomainModule, SharedModule, RouterTestingModule],
+            providers: [
+                {
+                    provide: ActivatedRoute, useValue: {
+                    params: Observable.of({caseid: '1234'}),
+                    snapshot: {
+                        data: {
+                            caseData: {
+                                sections: [],
+                                details: {
+                                    fields: [
+                                        {value: '123'},
+                                        {value: 'bob v bob'}
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                }
+                }
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DecisionRootComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
---
+++ src/app/routing/pages/hearings/root/root.component.spec.ts
@@ -0,0 +1,53 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {HearingRootComponent} from './root.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {DomainModule} from '../../../../domain/domain.module';
+import {SharedModule} from '../../../../shared/shared.module';
+import {Observable} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+
+describe('HearingRootComponent', () => {
+    let component: HearingRootComponent;
+    let fixture: ComponentFixture<HearingRootComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                HearingRootComponent
+            ],
+            imports: [DomainModule, SharedModule, RouterTestingModule],
+            providers: [
+                {
+                    provide: ActivatedRoute, useValue: {
+                    params: Observable.of({caseid: '1234'}),
+                    snapshot: {
+                        data: {
+                            caseData: {
+                                sections: [],
+                                details: {
+                                    fields: [
+                                        {value: '123'},
+                                        {value: 'bob v bob'}
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                }
+                }
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HearingRootComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});

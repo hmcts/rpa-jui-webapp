390f93bb6498434a37c724d8ba2e482c3ea9c1eb
Chelsea <cksanders1@gmail.com>
2018-08-07 11:17:37 +0100

    RIUI-363: Footer

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,43 +1,51 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {SharedModule} from '../shared/shared.module';
-import {DomainModule} from '../domain/domain.module';
-import {AuthGuardService} from '../auth/auth-guard.service';
-import {ViewCaseComponent} from './pages/view-case/view-case.component';
-import {HttpClientModule} from '@angular/common/http';
-
-import {RedirectionService} from './redirection.service';
-
-
-import {DecisionRootComponent} from "./pages/decisions/root/root.component";
-import {CreateDecisionComponent} from './pages/decisions/create-decision/create-decision.component';
-import {CheckDecisionComponent} from './pages/decisions/check-decision/check-decision.component';
-import {DecisionConfirmationComponent} from './pages/decisions/decision-confirmation/decision-confirmation.component';
-import {DecisionResolve} from "./resolve/decision.resolve";
-
-
-import {HearingRootComponent} from "./pages/hearings/root/root.component";
-import {CreateHearingComponent} from './pages/hearings/create-hearing/create-hearing.component';
-import {CheckHearingComponent} from './pages/hearings/check-hearing/check-hearing.component';
-import {HearingConfirmationComponent} from './pages/hearings/hearing-confirmation/hearing-confirmation.component';
-import {CaseResolve} from './resolve/case.resolve';
-
-
-import {CreateQuestionsComponent} from '../domain/components/questions/create/create.component';
-import {CheckQuestionsComponent} from '../domain/components/questions/check/check.component';
-import {ViewQuestionComponent} from '../domain/components/questions/view/view.component';
-import {DeleteQuestionComponent} from '../domain/components/questions/delete/delete.component';
-import {EditQuestionComponent} from '../domain/components/questions/edit/edit.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {JUIFormsModule} from "../forms/forms.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from '../shared/shared.module';
+import { DomainModule } from '../domain/domain.module';
+import { AuthGuardService } from '../auth/auth-guard.service';
+import { ViewCaseComponent } from './pages/view-case/view-case.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RedirectionService } from './redirection.service';
+import { DecisionRootComponent } from './pages/decisions/root/root.component';
+import { CreateDecisionComponent } from './pages/decisions/create-decision/create-decision.component';
+import { CheckDecisionComponent } from './pages/decisions/check-decision/check-decision.component';
+import { DecisionConfirmationComponent } from './pages/decisions/decision-confirmation/decision-confirmation.component';
+import { DecisionResolve } from './resolve/decision.resolve';
+import { HearingRootComponent } from './pages/hearings/root/root.component';
+import { CreateHearingComponent } from './pages/hearings/create-hearing/create-hearing.component';
+import { CheckHearingComponent } from './pages/hearings/check-hearing/check-hearing.component';
+import { HearingConfirmationComponent } from './pages/hearings/hearing-confirmation/hearing-confirmation.component';
+import { CaseResolve } from './resolve/case.resolve';
+import { CreateQuestionsComponent } from '../domain/components/questions/create/create.component';
+import { CheckQuestionsComponent } from '../domain/components/questions/check/check.component';
+import { ViewQuestionComponent } from '../domain/components/questions/view/view.component';
+import { DeleteQuestionComponent } from '../domain/components/questions/delete/delete.component';
+import { EditQuestionComponent } from '../domain/components/questions/edit/edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { JUIFormsModule } from '../forms/forms.module';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { CookiesComponent } from './pages/cookies/cookies.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         canActivate: [AuthGuardService]
+    },
+    {
+        path: 'terms-and-conditions',
+        component: TermsAndConditionsComponent
+    },
+    {
+        path: 'cookies',
+        component: CookiesComponent
+    },
+    {
+        path: 'privacy-policy',
+        component: PrivacyPolicyComponent
     },
     {
         path: 'viewcase/:case_id',
@@ -110,6 +118,9 @@ const routes: Routes = [
     ],
     declarations: [
         HomeComponent,
+        CookiesComponent,
+        PrivacyPolicyComponent,
+        TermsAndConditionsComponent,
         ViewCaseComponent,
         CreateDecisionComponent,
         CheckDecisionComponent,
@@ -129,6 +140,7 @@ const routes: Routes = [
         RouterModule
     ]
 })
+
 export class RoutingModule {
 }
 

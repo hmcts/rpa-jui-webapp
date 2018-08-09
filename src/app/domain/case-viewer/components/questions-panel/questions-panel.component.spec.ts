efaa38f8f393f24e6ea10ad1fc71ab0bc94cd2b5
Chelsea <cksanders1@gmail.com>
2018-08-09 14:25:47 +0100

    RIUI-317: view response from appellant

--- src/app/domain/case-viewer/components/questions-panel/questions-panel.component.spec.ts
+++ src/app/domain/case-viewer/components/questions-panel/questions-panel.component.spec.ts
@@ -132,9 +132,9 @@ describe('QuestionsPanelComponent', () => {
             it('should display two draft questions meta data', () => {
                 const metadata = nativeElement.querySelectorAll(Selector.selector('questions-meta-data'));
                 expect(metadata[0].textContent)
-                    .toBe('Last updated by 5899 at 8:52am on 13 July 2018');
+                    .toBe('Last updated by 5899 on 13 July 2018 at 8:52am');
                 expect(metadata[1].textContent)
-                    .toBe('Last updated by 5899 at 8:52am on 14 July 2018');
+                    .toBe('Last updated by 5899 on 14 July 2018 at 8:52am');
             });
 
             it('should display link to add more draft questions', () => {
@@ -326,6 +326,90 @@ describe('QuestionsPanelComponent', () => {
         it('should display a message to say when the questions were sent', () => {
             expect(nativeElement.querySelector(Selector.selector('sent-questions-details')).textContent)
                 .toEqual('You sent 2 questions to the appellant at 8:52am on 13 July 2018');
+        });
+    });
+
+    describe('When there are answers from appellant', () => {
+        beforeEach(async(() => {
+            TestBed
+                .configureTestingModule({
+                    imports: [
+                        CaseViewerModule,
+                        RouterTestingModule
+                    ],
+                    declarations: [],
+                    providers: [
+                        {
+                            provide: ActivatedRoute,
+                            useValue: mockRoute
+                        },
+                        {
+                            provide: ConfigService,
+                            useValue: mockConfigService
+                        }
+                    ]
+                })
+                .compileComponents();
+        }));
+
+        const data = {
+            'name': 'Questions',
+            'type': 'questions-panel',
+            'fields': [
+                {
+                    'value': [{
+                        'question_round_number': '1',
+                        'state': 'question_issue_pending',
+                        'questions': [
+                            {
+                                'id': 'be8ac935-ed7a-47b5-84ce-b5aa25e64512',
+                                'header': 'Test header 1',
+                                'body': 'Test body 1',
+                                'owner_reference': '5899',
+                                'state_datetime': new Date(Date.UTC(2018, 6, 13, 8, 52, 38)),
+                                'state': 'question_answered'
+                            },
+                            {
+                                'id': 'c7935438-b54c-4dad-bbe8-34fff72caf81',
+                                'header': 'Test header 2',
+                                'body': 'Test Header 2',
+                                'owner_reference': '5899',
+                                'state_datetime': new Date(Date.UTC(2018, 6, 14, 8, 52, 38)),
+                                'state': 'question_issued'
+                            }
+                        ]
+                    }]
+                }
+            ]
+        };
+
+        beforeEach(async(() => {
+            fixture = TestBed.createComponent(QuestionsPanelComponent);
+            component = fixture.componentInstance;
+            component.panelData = data;
+            component.caseId = '13eb9981-9360-4d4b-b9fd-506b5818e7ff';
+            nativeElement = fixture.nativeElement;
+            fixture.detectChanges();
+        }));
+
+        it('should create', () => {
+            expect(component)
+                .toBeTruthy();
+        });
+
+        it('should display sent questions', () => {
+            expect(nativeElement.querySelectorAll(Selector.selector('sent-questions-item')).length)
+                .toBe(2);
+        });
+
+        it('should display a message to say when the questions were sent', () => {
+            expect(nativeElement.querySelector(Selector.selector('questions-issued-meta-data')).textContent)
+                .toEqual('Sent by 5899 on 14 July 2018 at 8:52am');
+        });
+
+        it('should display a message to say when the answer was sent', () => {
+            expect(nativeElement.querySelector(Selector.selector('questions-answered-meta-data')).textContent)
+                .toEqual('Responded by Appellant on 13 July 2018 at 8:52am');
         });
     });
 });

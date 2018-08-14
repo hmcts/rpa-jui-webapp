976df7575fc3492c2b31a85a758d7e4516d74d94
Pawel Kaczmarek <pawel.kaczmarek@gmail.com>
2018-08-14 09:49:41 +0100

    switch to SIDAM again

--- config/environments/aat.config.js
+++ config/environments/aat.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     services: {
         ccd_data_api: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
-        // idam_web: "http://idam-web-public-idam-aat.service.core-compute-aat.internal",
-        // idam_api: "http://idam-api-idam-aat.service.core-compute-aat.internal",
-        idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
-        idam_web: 'http://idam.preprod.ccidam.reform.hmcts.net',
+        idam_web: "http://idam-web-public-idam-aat.service.core-compute-aat.internal",
+        idam_api: "http://idam-api-idam-aat.service.core-compute-aat.internal",
+        // idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
+        // idam_web: 'http://idam.preprod.ccidam.reform.hmcts.net',
         s2s: 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
         dm_store_api: 'http://dm-store-aat.service.core-compute-aat.internal',
         em_anno_api: 'http://em-anno-aat.service.core-compute-aat.internal',

d0578897cc93130dd9c53b4fd951a20186c083fb
Alec Doran-Twyford <alec@alectronic.co>
2018-08-17 14:02:22 +0100

    update SIDAM TIDAM config

--- config/environments/aat.config.js
+++ config/environments/aat.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     services: {
         ccd_data_api: 'https://ccd-data-store-api-aat.service.core-compute-aat.internal',
-        idam_web: 'https://idam-web-public.aat.platform.hmcts.net',
-        idam_api: 'https://idam-api.aat.platform.hmcts.net',
-        // idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
-        // idam_web: 'http://idam.preprod.ccidam.reform.hmcts.net',
+        // idam_web: 'https://idam-web-public.aat.platform.hmcts.net',
+        // idam_api: 'https://idam-api.aat.platform.hmcts.net',
+        idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
+        idam_web: 'http://idam.preprod.ccidam.reform.hmcts.net',
         s2s: 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
         dm_store_api: 'http://dm-store-aat.service.core-compute-aat.internal',
         em_anno_api: 'http://em-anno-aat.service.core-compute-aat.internal',
--- config/environments/demo.config.js
+++ config/environments/demo.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     services: {
         ccd_data_api: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal',
-        // idam_web: "http://idam-web-public-idam-demo.service.core-compute-demo.internal",
-        // idam_api: "http://idam-api-idam-demo.service.core-compute-demo.internal",
+        // idam_web: 'https://idam-web-public.aat.platform.hmcts.net',
+        // idam_api: 'https://idam-api.aat.platform.hmcts.net',
         idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
         idam_web: 'http://idam.preprod.ccidam.reform.hmcts.net',
         s2s: 'http://rpe-service-auth-provider-demo.service.core-compute-demo.internal',

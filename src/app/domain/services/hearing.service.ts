c003d676765e14276e9a7e88457a91902305ffce
shaibazkhan <sknizami@gmail.com>
2018-08-24 17:23:34 +0100

    added create hearing component

---
+++ src/app/domain/services/hearing.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {Observable, EMPTY, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {ConfigService} from '../../config.service';
+import {makeStateKey, TransferState} from '@angular/platform-browser';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class HearingService {
+
+    constructor(private httpClient: HttpClient, private configService: ConfigService, private state: TransferState) { }
+
+    generateHearingsUrl(caseId: string) {
+        return `${this.configService.config.api_base_url}/api/hearings/${caseId}`;
+    }
+
+    storeDraftHearing(caseId: string, text: string): Observable<any> {
+        const url = this.generateHearingsUrl(caseId);
+        const key = makeStateKey(url);
+
+        this.state.set(key, { relist_reason: text });
+
+        return EMPTY;
+    }
+
+    fetchDraftHearing(caseId: string): Observable<any> {
+        const url = this.generateHearingsUrl(caseId);
+        const key = makeStateKey(url);
+
+        const cache = this.state.get(key, null as any);
+        return (cache) ? of(cache) : of(null);
+    }
+}
--- src/app/routing/pages/hearings/create-hearing/create-hearing.component.ts
+++ src/app/routing/pages/hearings/create-hearing/create-hearing.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {HearingService} from '../../../../domain/services/hearing.service';
+import {RedirectionService} from '../../../redirection.service';
 
 @Component({
     selector: 'app-list-for-hearing',
@@ -7,18 +10,62 @@ import {ActivatedRoute, Router} from '@angular/router';
     styleUrls: ['./create-hearing.component.scss']
 })
 export class CreateHearingComponent implements OnInit {
+    form: FormGroup;
+    case: any;
+    hearing: any;
 
-    error = false;
+    relistReasonText: string = '';
+
+    eventEmitter: EventEmitter<any> = new EventEmitter();
+    callback_options = {
+        eventEmitter: this.eventEmitter
+    };
+
+    error = {
+        server: false,
+        notes: false
+    };
 
     constructor(
+        private fb: FormBuilder,
         private route: ActivatedRoute,
-        private router: Router
+        private router: Router,
+        public hearingService: HearingService,
+        public redirectionService: RedirectionService,
+        private cdRef: ChangeDetectorRef
     ) { }
 
-    ngOnInit() {
+    createForm() {
+        this.form = this.fb.group({
+            notes: [this.relistReasonText, Validators.required],
+        });
     }
 
-    submitHearing() {
-        this.router.navigate(['../check-hearings-notes'], {relativeTo: this.route});
+    ngOnInit() {
+        this.eventEmitter.subscribe(this.submitCallback.bind(this));
+
+        this.case = this.route.parent.snapshot.data['caseData'];
+        this.hearing = this.route.parent.snapshot.data['hearing'];
+
+        if (this.hearing) {
+            this.relistReasonText = this.hearing.relist_reason;
+        }
+        this.createForm();
+    }
+
+    ngAfterViewChecked() {
+        this.cdRef.detectChanges();
+    }
+
+    submitCallback(values) {
+        if (this.form.valid) {
+            this.hearingService.storeDraftHearing(this.case.id, values.notes)
+                .subscribe(
+                    () => this.redirectionService.redirect(`/jurisdiction/${this.case.case_jurisdiction}/casetype/${this.case.case_type_id}/viewcase/${this.case.id}/hearing/check`),
+                    error => this.error.server = true
+                );
+        } else {
+            this.error.notes = !this.form.controls.notes.valid;
+        }
     }
 }

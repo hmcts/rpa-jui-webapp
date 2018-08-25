398376aaed27d3790bea8453010f98b3d6874871
Alec Doran-Twyford <alec@alectronic.co>
2018-08-25 16:10:17 +0100

    coh event + timeline  (#164)

--- api/events/event.js
+++ api/events/event.js
@@ -22,11 +22,98 @@ function reduceEvents(events) {
     });
 }
 
-function getEvents(caseId, userId, jurisdiction, caseType, options) {
+function getHistory(arrObject) {
+   return arrObject.map(arr => arr.history)
+       .reduce((history, item) => {
+            return history.concat(item);
+        }, []);
+}
+function reduceCohEvents(events) {
+    return events.map(event => {
+        const createdDate = moment(event.state_datetime);
+        const dateUtc = createdDate.utc().format();
+        const date = createdDate.format('D MMM YYYY');
+        const time = createdDate.format('HH:mma');
+        return {
+            title: event.state_desc,
+            by: 'coh',
+            dateUtc,
+            date,
+            time,
+            documents: []
+        };
+    });
+}
+
+function mergeCohEvents(eventsJson) {
+        let history = eventsJson.online_hearing.history;
+        let questionHistory = eventsJson.online_hearing.questions ? getHistory(eventsJson.online_hearing.questions) : [];
+        let answersHistory = eventsJson.online_hearing.answers ? getHistory(eventsJson.online_hearing.answers): [];
+        let decisionHistory = eventsJson.online_hearing.decision ? eventsJson.online_hearing.decision.history: [];
+        return [...history, ...questionHistory, ...answersHistory, ...decisionHistory];
+}
+
+function sortEvents(events) {
+    return events.sort((result1, result2) => {
+        return moment.duration(moment(result2.dateUtc).diff(moment(result1.dateUtc))).asMilliseconds();
+    })
+}
+
+function getCcdEvents(caseId, userId, jurisdiction, caseType, options) {
     return generateRequest('GET', `${config.services.ccd_data_api}/caseworkers/${userId}/jurisdictions/${jurisdiction}/case-types/${caseType}/cases/${caseId}/events`, options)
         .then(events => reduceEvents(events));
 }
 
+function getCohEvents(caseId, userId, options) {
+    return getHearingId(caseId, userId, options)
+        .then(hearingId => {
+            return getOnlineHearingConversation(hearingId, options)
+                .then(mergeCohEvents)
+                .then(reduceCohEvents);
+        })
+}
+function combineLists(lists) {
+    return [].concat(...lists);
+}
+
+function getEvents(caseId, userId, jurisdiction, caseType, options) {
+    const promiseArray = [];
+     promiseArray.push(getCcdEvents(caseId, userId, jurisdiction, caseType, options));
+     promiseArray.push(getCohEvents(caseId, userId, options));
+      return Promise.all(promiseArray)
+          .then(combineLists)
+          .then(sortEvents);
+}
+
+function postHearing(caseId, userId, options, jurisdictionId = 'SSCS') {
+    const body = {
+        case_id: caseId,
+        jurisdiction: jurisdictionId,
+        panel: [{ identity_token: 'string', name: userId }],
+        start_date: (new Date()).toISOString()
+    };
+
+    return generateRequest('POST', `${config.services.coh_cor_api}/continuous-online-hearings`, options, body)
+        .then(hearing => hearing.online_hearing_id);
+}
+
+function getHearingId(caseId, userId, options) {
+    return generateRequest('GET', `${config.services.coh_cor_api}/continuous-online-hearings?case_id=${caseId}`, options)
+        .then(h => h.online_hearings[0] ? h.online_hearings[0].online_hearing_id : postHearing(caseId, userId, options));
+}
+
+function getOnlineHearingConversation(onlineHearingId, options) {
+    return generateRequest('GET',`${config.services.coh_cor_api}/continuous-online-hearings/${onlineHearingId}/conversations`, options);
+}
+
+function getOptions(req) {
+    return {
+        headers: {
+            'Authorization': `Bearer ${req.auth.token}`,
+            'ServiceAuthorization': req.headers.ServiceAuthorization
+        }
+    };
+}
 
 module.exports = app => {
     const router = express.Router({ mergeParams: true });
@@ -38,13 +125,7 @@ module.exports = app => {
         const jurisdiction = req.params.jur;
         const caseType = req.params.casetype;
 
-
-        getEvents(caseId, userId, jurisdiction, caseType, {
-            headers: {
-                Authorization: `Bearer ${req.auth.token}`,
-                ServiceAuthorization: req.headers.ServiceAuthorization
-            }
-        })
+        getEvents(caseId, userId, jurisdiction, caseType, getOptions(req))
             .then(events => {
                 res.setHeader('Access-Control-Allow-Origin', '*');
                 res.setHeader('content-type', 'application/json');
@@ -54,5 +135,4 @@ module.exports = app => {
 };
 
 module.exports.getEvents = getEvents;
-
 module.exports.reduceEvents = reduceEvents;
--- api/events/event.spec.js
+++ api/events/event.spec.js
@@ -3,23 +3,78 @@ const supertest = require('supertest');
 const express = require('express');
 const moment = require('moment');
 
+const utcDate4 = moment.utc('2018-08-06T16:14:11Z');
+const utcDate3 = moment.utc('2018-08-06T16:16:11Z');
+const utcDate2 = moment.utc('2018-08-22T11:54:48Z');
+const utcDate1 = moment.utc('2018-08-22T11:56:31Z');
+
 describe('Events route', () => {
-    let route, request, app;
-    let httpRequest, httpResponse;
+    let route;
+    let request;
+    let app;
+    let httpRequest;
+    let httpResponse;
+    let cohHearingIdResponse;
+    let cohEventsResponse;
 
     beforeEach(() => {
+        cohHearingIdResponse = {
+            online_hearings: [
+                {
+                    online_hearing_id: '5ea1ac54-406c-4e7e-bebe-76723672127c',
+                    case_id: '1534513630400666',
+                    start_date: utcDate1,
+                    panel: [{ name: '123141' }]
+                }
+            ]
+        };
+
         httpResponse = (resolve, reject) => {
             resolve([
                 {
                     event_name: 'event_name',
                     user_first_name: 'user_first_name',
                     user_last_name: 'user_last_name',
-                    created_date: '2018-08-06T16:14:11.898'
+                    created_date: utcDate1
                 }
             ]);
         };
+        cohEventsResponse = {
+            online_hearing: {
+                online_hearing_id: '5ea1ac54-406c-4e7e-bebe-76723672127c',
+                case_id: '1534513630400666',
+                start_date: utcDate1,
+                panel: [{ name: '123141' }],
+                current_state: {
+                    state_name: 'continuous_online_hearing_decision_issued',
+                    state_desc: 'Continuous Online Hearing Decision Issued',
+                    state_datetime: utcDate4
+                },
+                history: [
+                    {
+                        state_name: 'continuous_online_hearing_started',
+                        state_desc: 'Continuous Online Hearing Started',
+                        state_datetime: utcDate3
+                    },
+                    {
+                        state_name: 'continuous_online_hearing_decision_issued',
+                        state_desc: 'Continuous Online Hearing Decision Issued',
+                        state_datetime: utcDate4
+                    }
+                ],
+                uri: '/continuous-online-hearings/5ea1ac54-406c-4e7e-bebe-76723672127c'
+            }
+
+        };
         httpRequest = jasmine.createSpy();
-        httpRequest.and.callFake((url, options) => new Promise(httpResponse));
+        httpRequest.and.callFake((method, url) => {
+            if (url.includes('continuous-online-hearings?case_id=')) {
+                return Promise.resolve(cohHearingIdResponse);
+            } else if (url.includes('continuous-online-hearings/5ea1ac54-406c-4e7e-bebe-76723672127c/conversations')) {
+                return Promise.resolve(cohEventsResponse);
+            }
+            return Promise.resolve(httpResponse);
+        });
 
         app = express();
 
@@ -37,7 +92,6 @@ describe('Events route', () => {
 
     describe('getEvents', () => {
         let getEvents;
-        let utcDate = moment.utc('2018-08-06T16:14:11Z');
 
         beforeEach(() => {
             getEvents = route.getEvents;
@@ -51,40 +105,55 @@ describe('Events route', () => {
             expect(getEvents().then).toBeTruthy();
         });
 
-        it('should return all documents requested', done => {
-            httpResponse = (resolve, reject) => {
-                resolve([
-                    {
-                        event_name: 'event_name',
-                        user_first_name: 'user_first_name',
-                        user_last_name: 'user_last_name',
-                        created_date: moment.utc('2018-08-06T16:14:11.898').format()
-                    },
-                    {
-                        event_name: 'event_name2',
-                        user_first_name: 'user_first_name2',
-                        user_last_name: 'user_last_name2',
-                        created_date: moment.utc('2018-08-06T16:14:11.898').format()
-                    }
-                ]);
-            };
+        it('should return all documents requested for coh and ccd events', done => {
+            httpResponse = [
+                {
+                    event_name: 'event_name',
+                    user_first_name: 'user_first_name',
+                    user_last_name: 'user_last_name',
+                    created_date: utcDate1
+                },
+                {
+                    event_name: 'event_name2',
+                    user_first_name: 'user_first_name2',
+                    user_last_name: 'user_last_name2',
+                    created_date: utcDate2
+                }
+            ];
 
             getEvents().then(events => {
                 expect(events).toEqual([
                     {
                         title: 'event_name',
                         by: 'user_first_name user_last_name',
-                        dateUtc: utcDate.format(),
-                        date: '6 Aug 2018',
-                        time: utcDate.format('HH:mma'),
+                        dateUtc: utcDate1.utc().format(),
+                        date: utcDate1.format('D MMM YYYY'),
+                        time: utcDate1.format('HH:mma'),
                         documents: []
                     },
                     {
                         title: 'event_name2',
                         by: 'user_first_name2 user_last_name2',
-                        dateUtc: utcDate.format(),
-                        date: '6 Aug 2018',
-                        time: utcDate.format('HH:mma'),
+                        dateUtc: utcDate2.utc().format(),
+                        date: utcDate2.format('D MMM YYYY'),
+                        time: utcDate2.format('HH:mma'),
+                        documents: []
+                    },
+                    {
+                        title: 'Continuous Online Hearing Started',
+                        by: 'coh',
+                        dateUtc: utcDate3.utc().format(),
+                        date: utcDate3.format('D MMM YYYY'),
+                        time: utcDate3.format('HH:mma'),
+                        documents: []
+                    },
+                    {
+                        title: 'Continuous Online Hearing Decision Issued',
+                        by: 'coh',
+
+                        dateUtc: utcDate4.utc().format(),
+                        date: utcDate4.format('D MMM YYYY'),
+                        time: utcDate4.format('HH:mma'),
                         documents: []
                     }
                 ]);

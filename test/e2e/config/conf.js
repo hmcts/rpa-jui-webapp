a3d5aba022792199468eb7558eb045b09d7873ab
rramisetty <roopa.ramisetty@hmcts.net>
2018-09-27 15:12:40 +0100

    updated summarypage locators

--- test/e2e/config/conf.js
+++ test/e2e/config/conf.js
@@ -19,7 +19,7 @@ const jenkinsConfig = [
         acceptInsecureCerts: true,
         nogui: true,
 
-        chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] }
+        // chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] }
      }
 ];
 
@@ -38,7 +38,7 @@ const localConfig = [
         browserName: 'chrome',
         acceptInsecureCerts: true,
 
-        chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] },
+        // chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] },
         proxy: {
             proxyType: 'manual',
             httpProxy: 'proxyout.reform.hmcts.net:8080',
--- test/e2e/features/pages/caseSummaryPage.js
+++ test/e2e/features/pages/caseSummaryPage.js
@@ -2,8 +2,8 @@
 
 function caseSummaryPage() {
     this.caseSummary_header_text = element(by.css('[data-selector="heading"]'));
-    this.caseDetails_header_text = element.all(by.css(('[data-selector="title"]:nth-child(1)')));
-    this.casefields = element.all(by.css('[data-selector="table-header"]:nth-child(1)'));
+    this.caseDetails_header_text = element.all(by.css(('[data-selector="title"]')));
+    this.casefields = element.all(by.css('[data-selector="table-cell"]:nth-child(1)'));
 
 }
 

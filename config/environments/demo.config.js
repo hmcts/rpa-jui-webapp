042487d3fa79f9f400784c8967e04dda1f2eec98
Alec Doran-Twyford <alec@alectronic.co>
2018-11-21 15:58:35 +0000

    update ccd-def config for demo

--- config/environments/demo.config.js
+++ config/environments/demo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     services: {
         ccd_data_api: 'https://ccd-data-store-api-demo.service.core-compute-demo.internal',
-        ccd_def_api: 'https://ccd-definition-store-api-aat.service.core-compute-aat.internal',
+        ccd_def_api: 'https://ccd-definition-store-api-demo.service.core-compute-demo.internal',
         idam_web: 'https://idam.preprod.ccidam.reform.hmcts.net',
         idam_api: 'https://preprod-idamapi.reform.hmcts.net:3511',
         s2s: 'https://rpe-service-auth-provider-demo.service.core-compute-demo.internal',

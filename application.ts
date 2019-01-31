94361dac3451caa0199ac50502d5fd8e9411a79d
toireasam <t.moley@kainos.com>
2019-01-31 12:23:42 +0000

    Configuring helm charts (#582)
    * Configuring helm charts
    
    * Renaming helm chart folder
    
    * Renaming folder for charts
    
    * Changing nodejs version in helm chart config and adding decrypt key
    
    * Updating requirements with node js version
    
    * Removing kubernates config
    
    * Changing values template
    
    * Removing health check to test helm
    
    * ingress and application port changes

--- application.ts
+++ application.ts
@@ -75,16 +75,16 @@ app.get(
     "/health",
     healthcheck.configure({
         checks: {
-            ccd_data_api: healthcheckConfig(config.services.ccd_data_api),
+            //ccd_data_api: healthcheckConfig(config.services.ccd_data_api),
             // ccd_def_api: healthcheckConfig(config.services.ccd_def_api),
             // idam_web: healthcheckConfig(config.services.idam_web),
-            idam_api: healthcheckConfig(config.services.idam_api),
-            s2s: healthcheckConfig(config.services.s2s),
-            draft_store_api: healthcheckConfig(config.services.draft_store_api),
-            dm_store_api: healthcheckConfig(config.services.dm_store_api),
-            em_anno_api: healthcheckConfig(config.services.em_anno_api),
-            em_npa_api: healthcheckConfig(config.services.em_npa_api),
-            coh_cor_api: healthcheckConfig(config.services.coh_cor_api)
+            //idam_api: healthcheckConfig(config.services.idam_api),
+            //s2s: healthcheckConfig(config.services.s2s),
+            //draft_store_api: healthcheckConfig(config.services.draft_store_api),
+            //dm_store_api: healthcheckConfig(config.services.dm_store_api),
+            //em_anno_api: healthcheckConfig(config.services.em_anno_api),
+            //em_npa_api: healthcheckConfig(config.services.em_npa_api),
+            //coh_cor_api: healthcheckConfig(config.services.coh_cor_api)
         },
         buildInfo: {}
     })

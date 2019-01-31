8eace88b23e47536f9bc4e04464b88b03357d64e
Dmitry Panov <dpanov76@yahoo.com>
2019-01-31 16:34:14 +0000

    Date validation now can display one message instead of many at the same time

--- src/app/shared/components/date/date.component.ts
+++ src/app/shared/components/date/date.component.ts
@@ -13,8 +13,18 @@ export class DateComponent {
     @Input() showValidation;
     @Input() validationError;
 
-    displayOneMessage(errors, controlId, errorId){
-        console.log(errors, controlId, errorId);
+    displayOneMessage(errors, controlId){
+        console.log(errors);
+        if(errors){
+            const keys = Object.keys(errors);
+            console.log(keys);
+            for (const key of keys) {
+                if (key.includes(controlId)) {
+                    console.log( "found", key);
+                    return key;
+                }
+            }
+        }
     }
 
 }

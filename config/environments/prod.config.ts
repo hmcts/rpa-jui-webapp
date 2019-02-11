898b15bb9f3addf205fae3346852ab78c27a47f8
toireasam <t.moley@kainos.com>
2019-02-11 19:51:06 +0000

    Update prod.config.ts (#650)

--- config/environments/prod.config.ts
+++ config/environments/prod.config.ts
@@ -19,6 +19,6 @@ export default {
         coh_cor_api: 'https://coh-cor-prod.service.core-compute-prod.internal'
     },
     useProxy: false,
-    secureCookie: false,
+    secureCookie: true,
     sessionSecret: 'secretSauce'
 };

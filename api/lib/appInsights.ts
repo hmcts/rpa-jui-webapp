2d4c8d4c34a3f1ef3df121ea9d54347ebfa9c577
toireasam <t.moley@kainos.com>
2019-02-12 21:06:23 +0000

    Update appInsights.ts (#657)

--- api/lib/appInsights.ts
+++ api/lib/appInsights.ts
@@ -7,7 +7,7 @@ export let client
 // shouldnt do this check here but this is a high level dep
 const environment = process.env.JUI_ENV || 'local'
 
-if (environment !== 'local' && environment !== 'aat') {
+if (environment !== 'local') {
     applicationinsights
         .setup(config.appInsightsInstrumentationKey)
         .setAutoDependencyCorrelation(true)

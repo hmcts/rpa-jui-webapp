59f558a7ecc9039731514d09ddb05dbf78fc229c
smogese <43000328+smogese@users.noreply.github.com>
2019-02-26 14:43:13 +0000

    Revert "no ng tunnel (#745)" (#746)
    This reverts commit 49fd9b6df33aba642ce94680341b70057b67d3dc.

--- api/lib/http/index.ts
+++ api/lib/http/index.ts
@@ -1,22 +1,8 @@
 import axios, { AxiosInstance } from 'axios'
-import * as  axiosHttpsProxy from 'axios-https-proxy'
-import { config } from '../../../config'
 import { errorInterceptor, requestInterceptor, successInterceptor } from '../interceptors'
 
-const options = config.proxy ? {
-    proxy: {
-        host: config.proxy.host,
-        port: config.proxy.port,
-    },
-} : {}
-
-export const http: AxiosInstance = axios.create(options)
+export const http: AxiosInstance = axios.create({})
 
 axios.defaults.headers.common['Content-Type'] = 'application/json'
-
-if (config.proxy) {
-    http.interceptors.request.use(axiosHttpsProxy)
-}
-
 http.interceptors.request.use(requestInterceptor)
 http.interceptors.response.use(successInterceptor, errorInterceptor)
--- api/lib/request/proxy.ts
+++ api/lib/request/proxy.ts
@@ -1,11 +1,10 @@
-import { config } from '../../../config'
 let _agent
 
-export default configuration => {
+module.exports = configuration => {
     if (!_agent) {
         const SocksProxyAgent = require('socks-proxy-agent')
 
-        const proxyUrl = `http://${config.proxy.host}:${config.proxy.port}`
+        const proxyUrl = 'socks://127.0.0.1:9090'
         _agent = new SocksProxyAgent(proxyUrl, true)
     }
     configuration.agent = _agent

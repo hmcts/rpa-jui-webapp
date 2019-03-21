6acc58bb635489cbe3f90ae488d5955e645f1ca5
Dmitry Panov <dpanov76@yahoo.com>
2019-03-21 15:02:01 +0000

    app.component unit tests fix (#849)
    * app.component unit tests fix
    
    * fixed code duplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from './config.service';
 import { NavigationEnd, Router, Event } from '@angular/router';
-import { environment } from '../environments/environment';
 
 @Component({
     selector: 'app-root',
@@ -23,27 +22,9 @@ export class AppComponent implements OnInit {
             }
         });
 
-        if (environment.googleAnalyticsKey) {
-            // this.appendGaTrackingCode(); // TODO: fix analytics
-        }
     }
 
-    private appendGaTrackingCode() {
-        try {
-            const script = document.createElement('script');
-            script.innerHTML = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-            ga('create', '${environment.googleAnalyticsKey}', 'auto');`;
-            document.head.appendChild(script);
-        } catch (ex) {
-            console.error('Error appending google analytics');
-            console.error(ex);
-        }
-    }
-
-    private replacedTitles(url: string): string {
+    replacedTitles(url: string): string {
         if (url.indexOf('summary') !== -1) {
             return 'summary';
         }
@@ -68,7 +49,7 @@ export class AppComponent implements OnInit {
         return '/';
     }
 
-    private getTitle(key): string {
+    getTitle(key): string {
         const titleMapping: { [id: string]: string } = {
             '/': 'Your cases - Judicial case manager',
             'summary': 'Summary - Judicial case manager',

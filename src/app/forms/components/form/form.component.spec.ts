0307f26fc9419eb5a36c003a0c01b2742df61cc6
Dmitry Panov <dpanov76@yahoo.com>
2019-04-04 11:52:13 +0100

    form.component.ts unit tests fixed (#899)
    * Form.component.ts fixed unit tests up to 96%
    
    * fdescribe removed
    
    * removed unused dependencies

--- src/app/forms/components/form/form.component.spec.ts
+++ src/app/forms/components/form/form.component.spec.ts
@@ -1,25 +1,69 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import {FormControl, FormGroup} from '@angular/forms';
+import {FormService} from '../../services/form.service';
+import {Listener} from 'selenium-webdriver';
+import {HostListener} from '@angular/core';
 
 describe('FormComponent', () => {
-  let component: FormComponent;
-  let fixture: ComponentFixture<FormComponent>;
+    let component: FormComponent;
+    let fixture: ComponentFixture<FormComponent>;
+    let formService;
+    let mockFormGroup;
+    let button;
+    const newTemplate = `
+        <form method="post" action="">
+            <button>MockButton</button>
+        </form>`;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ FormComponent ],
+            providers: [{
+                provide: FormService,
+                useValue: {
+                    getFormValues: () => {
+                        return {
+                            testFormControl1: 'testValue1',
+                            testFormControl2: 'testValue2'
+                        };
+                    }
+                }
+            }]
+        }).overrideTemplate(FormComponent, newTemplate )
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FormComponent);
+        component = fixture.componentInstance;
+        formService = fixture.debugElement.injector.get(FormService);
+        mockFormGroup = new FormGroup({
+            testFormControl1: new FormControl(),
+            testFormControl2: new FormControl()
+        });
+        component.formGroup = mockFormGroup;
+        button = fixture.nativeElement.querySelector('button');
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should call submitListener', () => {
+        console.log(component.submitListener());
+        expect(component.submitListener()).toBe(undefined);
+    });
+
+    it('should emit call back when triggerCallback called', () => {
+        component.callback_options = {
+            eventEmitter: {
+                emit: () => false
+            }
+        };
+        component.triggerCallback(mockFormGroup);
+        expect(component.callback_options.eventEmitter.emit(mockFormGroup)).toBe(false);
+    });
 });

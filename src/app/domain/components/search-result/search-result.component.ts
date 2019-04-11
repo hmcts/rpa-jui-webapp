df56c9ccf1bd5c54ca230c6222ea97d729092c56
Philip <philip.aberneithy@gmail.com>
2019-04-11 14:55:02 +0100

    Agreed with Product Owner, and UX on functionality, look and feel of the error message on the initial case list page.

--- src/app/domain/components/search-result/search-result.component.ts
+++ src/app/domain/components/search-result/search-result.component.ts
@@ -60,7 +60,7 @@ export class SearchResultComponent implements OnInit {
         this.minimalErrorStack = this.errorFormattingService.createMinimalErrorStack(errorStack.error);
         this.humanReadableErrorStack = this.errorFormattingService.createHumanReadableStack(this.minimalErrorStack);
 
-        this.browserTime = moment().format();
+        this.browserTime = moment().format(`MMMM Do YYYY, h:mm:ss a`);
 
         console.log('HttpErrorResponse Error:');
         console.log(errorStack);

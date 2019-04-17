f28e33d7c3841fa9ed76ebc18c1f14bc91dc96d9
Kristaps Tarasovs <31708534+frontr-uk@users.noreply.github.com>
2019-04-17 11:39:17 +0100

    [BLOCKED]: Tests middleware (#909)
    * Test: Introduce tests for Headers
    
    * refactor scope object to methods, add all in file
    
    * 'AddingTests'
    
    * 're-add-packaglock'
    
    * Update package-lock conflicts
    
    * Refactoring scope name

--- api/lib/middleware/serviceToken.ts
+++ api/lib/middleware/serviceToken.ts
@@ -5,11 +5,11 @@ import * as log4jui from '../log4jui'
 import { asyncReturnOrError } from '../util'
 
 const logger = log4jui.getLogger('service-token')
-
+const that = this
 const _cache = {}
 const microservice = config.microservice
 
-function validateCache() {
+export function validateCache() {
     logger.info('validaing s2s cache')
     const currentTime = Math.floor(Date.now() / 1000)
     if (!_cache[microservice]) {
@@ -18,11 +18,11 @@ function validateCache() {
     return currentTime < _cache[microservice].expiresAt
 }
 
-function getToken() {
+export function getToken() {
     return _cache[microservice]
 }
 
-async function generateToken() {
+export async function generateToken() {
     logger.info('Getting new s2s token')
     const token = await postS2SLease()
 
@@ -36,13 +36,13 @@ async function generateToken() {
     return token
 }
 
-async function serviceTokenGenerator() {
-    if (validateCache()) {
+export async function serviceTokenGenerator() {
+    if (that.validateCache()) {
         logger.info('Getting cached s2s token')
         const tokenData = getToken()
         return tokenData.token
     } else {
-        return await generateToken()
+        return await that.generateToken()
     }
 }
 

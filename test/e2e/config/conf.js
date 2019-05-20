aa37c78e9db8bba57c95129606f661d6c73eba0a
Alan <alan.work@teamsmog.com>
2019-05-20 17:09:58 +0100

    disabled tests  temporarily

--- test/e2e/config/conf.js
+++ test/e2e/config/conf.js
@@ -1,6 +1,6 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-const minimist = require('minimist');
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const minimist = require("minimist");
 
 const argv = minimist(process.argv.slice(2));
 
@@ -14,10 +14,19 @@ const jenkinsConfig = [
     // },
 
     {
-        browserName: 'chrome',
+        browserName: "chrome",
         acceptInsecureCerts: true,
         nogui: true,
-        chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote ', '--disableChecks'] }
+        chromeOptions: {
+            args: [
+                "--headless",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-setuid-sandbox",
+                "--no-zygote ",
+                "--disableChecks"
+            ]
+        }
     }
 ];
 
@@ -33,43 +42,50 @@ const localConfig = [
     //     }
     //  },
     {
-        browserName: 'chrome',
+        browserName: "chrome",
         acceptInsecureCerts: true,
-        chromeOptions: { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-zygote '] },
+        chromeOptions: {
+            args: [
+                "--headless",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-setuid-sandbox",
+                "--no-zygote "
+            ]
+        },
         proxy: {
-            proxyType: 'manual',
-            httpProxy: 'proxyout.reform.hmcts.net:8080',
-            sslProxy: 'proxyout.reform.hmcts.net:8080',
-            noProxy: 'localhost:3000'
+            proxyType: "manual",
+            httpProxy: "proxyout.reform.hmcts.net:8080",
+            sslProxy: "proxyout.reform.hmcts.net:8080",
+            noProxy: "localhost:3000"
         }
     }
 ];
 
-const cap = (argv.local) ? localConfig : jenkinsConfig;
+const cap = argv.local ? localConfig : jenkinsConfig;
 
 const config = {
-    framework: 'custom',
-    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    framework: "custom",
+    frameworkPath: require.resolve("protractor-cucumber-framework"),
     //specs: ['../features/**/*.feature'],
     specs: [
-        '../features/**/caseFile.feature',
-        '../features/**/login.feature',
-        '../features/**/makeDecision.feature',
-        '../features/**/parties.feature',
-        '../features/**/questions.feature',
-        '../features/**/recentEvents.feature',
+        // '../features/**/caseFile.feature',
+        // '../features/**/login.feature',
+        // '../features/**/makeDecision.feature',
+        // '../features/**/parties.feature',
+        // '../features/**/questions.feature',
+        // '../features/**/recentEvents.feature',
     ],
-    baseUrl: process.env.TEST_URL || 'http://localhost:3000/',
+    baseUrl: process.env.TEST_URL || "http://localhost:3000/",
     params: {
-        serverUrls: process.env.TEST_URL || 'http://localhost:3000/',
-        targetEnv: argv.env || 'local',
+        serverUrls: process.env.TEST_URL || "http://localhost:3000/",
+        targetEnv: argv.env || "local",
         username: process.env.TEST_EMAIL,
         password: process.env.TEST_PASSWORD,
         fr_judge_username: process.env.FR_EMAIL,
         fr_judge_password: process.env.FR_PASSWORD,
         sscs_username: process.env.SSCS_EMAIL,
         sscs_password: process.env.SSCS_PASSWORD
-
     },
     directConnect: true,
     // seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -91,32 +107,32 @@ const config = {
     cucumberOpts: {
         strict: true,
         // format: ['node_modules/cucumber-pretty'],
-        format: ['node_modules/cucumber-pretty', 'json:reports_json/results.json'],
-        tags: ['@all'],
+        format: [
+            "node_modules/cucumber-pretty",
+            "json:reports_json/results.json"
+        ],
+        tags: ["@all"],
         require: [
-            '../support/timeout.js',
-            '../support/world.js',
-            '../support/*.js',
-            '../features/step_definitions/**/*.steps.js'
+            "../support/timeout.js",
+            "../support/world.js",
+            "../support/*.js",
+            "../features/step_definitions/**/*.steps.js"
         ]
     },
 
     plugins: [
         {
-            package: 'protractor-multiple-cucumber-html-reporter-plugin',
+            package: "protractor-multiple-cucumber-html-reporter-plugin",
             options: {
                 automaticallyGenerateReport: true,
                 removeExistingJsonReportFile: true,
-                reportName: 'JUI Functional Tests',
+                reportName: "JUI Functional Tests",
                 // openReportInBrowser: true,
-                jsonDir: 'reports/tests/functional',
-                reportPath: 'reports/tests/functional'
+                jsonDir: "reports/tests/functional",
+                reportPath: "reports/tests/functional"
             }
         }
     ]
-
-
 };
-
 
 exports.config = config;

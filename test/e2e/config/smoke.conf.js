0e475e68f1b172527b329e5c28e3e10d1e0e6617
Alan McCabe-Jones <alan@localhost.localdomain>
2019-05-29 09:16:15 +0100

    don't disable smoke tests

--- test/e2e/config/smoke.conf.js
+++ test/e2e/config/smoke.conf.js
@@ -70,14 +70,14 @@ const config = {
     frameworkPath: require.resolve("protractor-cucumber-framework"),
     //specs: ['../features/**/*.feature'],
     specs: [
-        // '../features/**/caseFile.feature',
-        // '../features/**/login.feature',
-        // '../features/**/dashBoard.min.feature',
-        // '../features/**/makeDecision.feature',
-        // '../features/**/parties.feature',
-        // '../features/**/questions.feature',
-        // '../features/**/recentEvents.feature',
-        // '../features/**/summary.feature'
+        '../features/**/caseFile.feature',
+        '../features/**/login.feature',
+        '../features/**/dashBoard.min.feature',
+        '../features/**/makeDecision.feature',
+        '../features/**/parties.feature',
+        '../features/**/questions.feature',
+        '../features/**/recentEvents.feature',
+        '../features/**/summary.feature'
     ],
 
     baseUrl: process.env.TEST_URL || "http://localhost:3000/",

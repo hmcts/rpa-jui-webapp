5dd17fd286027d79c7f2190410888952bd197307
Toireasa Moley <toireasa.moley@hmcts.net>
2019-05-29 13:30:35 +0100

    Updating infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -46,9 +46,6 @@ module "app" {
         PACKAGES_PROJECT = "${var.team_name}"
         PACKAGES_ENVIRONMENT = "${var.env}"
 
-        S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-        IDAM_SECRET = "${data.azurerm_key_vault_secret.oauth2_secret.value}"
-        DECRYPT_KEY = "${data.azurerm_key_vault_secret.decrypt_key.value}"
     }
 }
 

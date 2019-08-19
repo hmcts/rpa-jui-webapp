cd57216307f1e5f2a8ae3843f612d672a5c18ab2
Pardeep Singh Basi <paddybasi@gmail.com>
2019-08-19 13:55:51 +0100

    changed draft store API config to http (#1015)
    * added console.log
    
    * added console.log
    
    * changed draft store api to use http instead of https

--- config/environments/local.config.ts
+++ config/environments/local.config.ts
@@ -9,7 +9,7 @@ export default {
         s2s:
             'https://rpe-service-auth-provider-aat.service.core-compute-aat.internal',
         draft_store_api:
-            'https://draft-store-service-aat.service.core-compute-aat.internal',
+            'http://draft-store-service-aat.service.core-compute-aat.internal',
         dm_store_api: 'https://dm-store-aat.service.core-compute-aat.internal',
         em_anno_api: 'https://em-anno-aat.service.core-compute-aat.internal',
         em_npa_api: 'https://em-npa-aat.service.core-compute-aat.internal',

b87e7adab351085d7ae203ee3af6023f2ffc6069
Philip <philip.aberneithy@gmail.com>
2019-09-05 10:30:25 +0100

    Attempting to ignore @component block.

--- src/app/routing/pages/page-not-found/page-not-found.component.ts
+++ src/app/routing/pages/page-not-found/page-not-found.component.ts
@@ -5,6 +5,7 @@ import {Component} from '@angular/core';
     templateUrl: './page-not-found.component.html',
     styleUrls: ['./page-not-found.component.scss']
 })
+//IgnoreTest
 export class PageNotFoundComponent {
     constructor() {
     }

bc17acb11584a8777cbe645030d2b63e83df24e3
Pardeep Singh Basi <paddybasi@gmail.com>
2019-09-19 15:02:28 +0100

    updated redis-cache module source in terraform (#1038)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -54,7 +54,7 @@ module "app" {
 }
 
 module "redis-cache" {
-    source      = "git@github.com:contino/moj-module-redis?ref=master"
+    source      = "git@github.com:hmcts/cnp-module-redis?ref=master"
     product     = "${var.product}-redis"
     location    = "${var.location}"
     env         = "${var.env}"
